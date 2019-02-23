f2cfde782bcebb1b0cdf49055062cbe9aaa64805
eiyuu-no-monogatari <46346528+eiyuu-no-monogatari@users.noreply.github.com>
2019-02-24 06:42:18 +0800

    Update bot.js

--- BOT2/bot.js
+++ BOT2/bot.js
@@ -56,7 +56,7 @@ client.on('message', msg => {
                             msgB = (msg.mentions.users.first() //1st
                                 + ' ＶＳ ' + userWhoGotTagged//2nd
                                 + "！");
-                            let embedA = new Discord.RichEmbed();
+                            const embedA = new Discord.RichEmbed();
                             embedA.setColor(0xFF0000);
                             embedA.addBlankField();
                             embedA.addField(msg.mentions.users.first().username, 750 + "/750", true);
@@ -78,7 +78,7 @@ client.on('message', msg => {
                             second_attack = msg.mentions.users.first().username;
                             msgA = '尼祿祭第 ' + a.toString() + ' 演技，開演！';
                             msgB = (msg.author.toString() + ' ＶＳ ' + userWhoGotTagged + "！");
-                            let embedA = new Discord.RichEmbed();
+                            const embedA = new Discord.RichEmbed();
                             embedA.setColor(0xFF0000);
                             embedA.addBlankField();
                             embedA.addField(msg.author.username, 750 + "/750", true);
@@ -104,7 +104,7 @@ client.on('message', msg => {
                         second_attack = msg.author.username;
                         msgA = '尼祿祭第 ' + a.toString() + ' 演技，開演！';
                         msgB = (msg.author.toString() + ' ＶＳ ' + userWhoGotTagged + "！");
-                        let embedA = new Discord.RichEmbed();
+                        const embedA = new Discord.RichEmbed();
                         embedA.setColor(0xFF0000);
                         embedA.addBlankField();
                         embedA.addField(msg.author.username, 750 + "/750", true);
