3fe8234d1756d912013d918ef021a4723160fa06
eiyuu-no-monogatari <46346528+eiyuu-no-monogatari@users.noreply.github.com>
2019-02-24 06:40:31 +0800

    Update bot.js

--- BOT2/bot.js
+++ BOT2/bot.js
@@ -17,9 +17,6 @@ var np_switch = true;
 msgA = "";
 msgB = "";
 var switchB = false;
-embedA = new Discord.RichEmbed();
-embedB = new Discord.RichEmbed();
-
 
 const express = require('express')
 var port = process.env.PORT || 5000;
@@ -59,7 +56,7 @@ client.on('message', msg => {
                             msgB = (msg.mentions.users.first() //1st
                                 + ' ＶＳ ' + userWhoGotTagged//2nd
                                 + "！");
-                            var embedA = new Discord.RichEmbed();
+                            let embedA = new Discord.RichEmbed();
                             embedA.setColor(0xFF0000);
                             embedA.addBlankField();
                             embedA.addField(msg.mentions.users.first().username, 750 + "/750", true);
@@ -81,7 +78,7 @@ client.on('message', msg => {
                             second_attack = msg.mentions.users.first().username;
                             msgA = '尼祿祭第 ' + a.toString() + ' 演技，開演！';
                             msgB = (msg.author.toString() + ' ＶＳ ' + userWhoGotTagged + "！");
-                            var embedA = new Discord.RichEmbed();
+                            let embedA = new Discord.RichEmbed();
                             embedA.setColor(0xFF0000);
                             embedA.addBlankField();
                             embedA.addField(msg.author.username, 750 + "/750", true);
@@ -107,7 +104,7 @@ client.on('message', msg => {
                         second_attack = msg.author.username;
                         msgA = '尼祿祭第 ' + a.toString() + ' 演技，開演！';
                         msgB = (msg.author.toString() + ' ＶＳ ' + userWhoGotTagged + "！");
-                        var embedA = new Discord.RichEmbed();
+                        let embedA = new Discord.RichEmbed();
                         embedA.setColor(0xFF0000);
                         embedA.addBlankField();
                         embedA.addField(msg.author.username, 750 + "/750", true);
@@ -130,30 +127,25 @@ client.on('message', msg => {
 
                     function Embed_battle(action) {
                         var arr = msg.mentions.users.array();
+                        const embedB = new Discord.RichEmbed();
                         if (msg.content == "--NeroFes") {
-                            var embedB = new Discord.RichEmbed();
                             embedB.setColor(0xFF0000);
                             embedB.addBlankField();
                             embedB.addField(msg.author.username, healthB + "/750", true);
                             embedB.addField(userWhoGotTagged_COPY, healthA + "/750", true);
                             embedB.setDescription("");
-                            return embedB;
                         } else if (arr.length == 2) {
-                            var embedB = new Discord.RichEmbed();
                             embedB.setColor(0xFF0000);
                             embedB.addBlankField();
                             embedB.addField(msg.mentions.users.first().username, healthB + "/750", true);
                             embedB.addField(msg.mentions.users.last().username, healthA + "/750", true);
                             embedB.setDescription(action);
-                            return embedB;
                         } else if (arr.length == 1) {
-                            var embedB = new Discord.RichEmbed();
                             embedB.setColor(0xFF0000);
                             embedB.addBlankField();
                             embedB.addField(msg.author.username, healthB + "/750", true);
                             embedB.addField(userWhoGotTagged_COPY, healthA + "/750", true);
                             embedB.setDescription(action);
-                            return embedB;
                         } else {
                             msg.channel.send("ERROR(3).");
                         }
